4a497087c5f6dc284b224e3b3c4738c0cae0a3d7
Andrew Hernandez <jandrewh@outlook.com>
2019-11-04 15:01:28 -0800

    added code from callbackLED to main.ino - still need to update switchcharacteristics

--- source/main.ino
+++ source/main.ino
@@ -26,6 +26,7 @@
 
 #include <SparkFun_Bio_Sensor_Hub_Library.h>
 #include <Wire.h>
+#include <ArduinoBLE.h>
 
 // Reset pin, MFIO pin
 int resPin = 4;
@@ -49,10 +50,49 @@ bioData body;
 // body.status     - Has a finger been sensed?
 
 
+BLEService heartService("19B10000-E8F2-537E-4F6C-D104768A1214"); // create service
+
+// create switch characteristic and allow remote device to read and write
+BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+
+
 void setup(){
 
   Serial.begin(115200);
 
+    // begin BLE initialization
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
+
+    while (1);
+  }
+
+  // set the local name peripheral advertises
+  BLE.setLocalName("HeartRateCallback");
+  // set the UUID for the service this peripheral advertises
+  BLE.setAdvertisedService(heartService);
+
+  // add the characteristic to the service
+  heartService.addCharacteristic(switchCharacteristic);
+
+  // add service
+  BLE.addService(heartService);
+
+  // assign event handlers for connected, disconnected to peripheral
+  BLE.setEventHandler(BLEConnected, blePeripheralConnectHandler);
+  BLE.setEventHandler(BLEDisconnected, blePeripheralDisconnectHandler);
+
+  // assign event handlers for characteristic
+  switchCharacteristic.setEventHandler(BLEWritten, switchCharacteristicWritten);
+  // set an initial value for the characteristic
+  switchCharacteristic.setValue(0);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println(("Bluetooth device active, waiting for connections..."));
+
+/////heart initialization
   Wire.begin();
   int result = bioHub.begin();
   if (result == 0) // Zero errors!
@@ -95,4 +135,36 @@ void loop(){
     // Slow it down or your heart rate will go up trying to keep up
     // with the flow of numbers
     delay(250); 
+}
+
+
+///////ble functions
+
+
+
+void blePeripheralConnectHandler(BLEDevice central) {
+  // central connected event handler
+  Serial.print("Connected event, central: ");
+  Serial.println(central.address());
+}
+
+void blePeripheralDisconnectHandler(BLEDevice central) {
+  // central disconnected event handler
+  Serial.print("Disconnected event, central: ");
+  Serial.println(central.address());
+}
+
+
+///change this to heart stuff!
+void switchCharacteristicWritten(BLEDevice central, BLECharacteristic characteristic) {
+  // central wrote new value to characteristic, update LED
+  Serial.print("Characteristic event, written: ");
+
+  if (switchCharacteristic.value()) {
+    Serial.println("LED on");
+    digitalWrite(ledPin, HIGH);
+  } else {
+    Serial.println("LED off");
+    digitalWrite(ledPin, LOW);
+  }
 }
